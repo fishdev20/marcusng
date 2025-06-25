897a1cf9e4ae1aae9ab54b2788e01443acb592c5
Marcus <minh.nguyen@triona.fi>
2025-06-25 14:27:44 +0300

    rm experience section

--- app/pages/home/ExperienceSection.tsx
+++ app/pages/home/ExperienceSection.tsx
@@ -1,16 +1,8 @@
-import { ExperienceContent } from "@/app/components/experience/ExperienceContent";
-import { Timeline } from "@/components/ui/timeline";
-import { experienceData } from "@/constants/experience";
-
 export default function ExperienceSection() {
-  const timelineData = experienceData.map((item) => ({
-    duration: item.duration,
-    content: <ExperienceContent item={item} />,
-  }));
+  // const timelineData = experienceData.map((item) => ({
+  //   duration: item.duration,
+  //   content: <ExperienceContent item={item} />,
+  // }));
 
-  return (
-    <section className="relative w-full overflow-hidden max-w-7xl mx-auto px-6 md:px-16">
-      <Timeline data={timelineData} />
-    </section>
-  );
+  return null;
 }
