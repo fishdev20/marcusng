c0b5589708db7cced153f1d64c5aaf4c916f6357
Marcus <minh.nguyen@triona.fi>
2025-07-15 14:38:12 +0300

    update meta data

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,16 +1,27 @@
 "use client";
 
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import black_logo from "../../assets/images/black_logo.png";
 import white_logo from "../../assets/images/white_logo.png";
 
-import { useTheme } from "next-themes";
-import Image from "next/image";
-
 export default function Logo() {
-  const { theme } = useTheme();
-  if (theme === "dark") {
-    return <Image src={white_logo} width={35} height={35} alt="logo" />;
-  }
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  return <Image src={black_logo} width={35} height={35} alt="logo" />;
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Image
+      src={resolvedTheme === "dark" ? white_logo : black_logo}
+      width={35}
+      height={35}
+      alt="logo"
+    />
+  );
 }
