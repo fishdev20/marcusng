42dc85f823a4c1379436c59ef5aeaa2f50c06362
Marcus <minh.nguyen@triona.fi>
2025-10-23 23:00:02 +0300

    check key

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,9 +2,8 @@ import { ContactEmail } from "@/app/components/ContactEmail";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
-
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY!);
   try {
     const { firstName, lastName, email, message } = await req.json();
 
